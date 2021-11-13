874dd23aed6e691bb3de110c0e252892becab045
axel <axel@pop-os.localdomain>
2021-11-13 02:33:14 +0100

    Implementing autocomplete logic + design

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
 /*========= LocalStorage ========= */
 let characters = JSON.parse(localStorage.getItem('characters')) || []
 
+/*========= DOM elements ========= */
+const searchbar = document.querySelector('.searchbar')
+const autocomplete_container = document.querySelector('.autocomplete-results')
+const characters_container = document.querySelector('.characters')
+const load_more_btn = document.querySelector('.load-more-btn')
+
 /*========= Variables ========= */
 let oldArray;
 let pageNumber  = 0;
@@ -32,8 +38,6 @@ const filterCharacters = () => {
 }
 
 const renderCharacterCard  = characters => {
-    const characters_container = document.querySelector('.characters')
-
     characters_container.innerHTML = ''
 
     for(const character of characters) {
@@ -54,16 +58,47 @@ const renderCharacterCard  = characters => {
     }
 }
 
+const searchAutoComplete = () => {
+    const search_value = searchbar.value
+    const suggestions = characters.filter(character => character.name.toLowerCase().includes(search_value.toLowerCase()))
+
+    for(const suggestion of suggestions) {
+        const autocomplete_result = document.createElement('li')
+
+        autocomplete_result.textContent = suggestion.name
+        autocomplete_result.className = 'autocomplete-result'
+        autocomplete_result.addEventListener('click', e => {
+            searchbar.value = e.target.textContent
+            filterCharacters()
+            autocomplete_container.classList.remove('show-autocomplete')
+
+        })
+
+        autocomplete_container.appendChild(autocomplete_result)
+    }
+
+    autocomplete_container.classList.add('show-autocomplete')
+}
+
 /*========= Event Listeners ========= */
-const searchbar = document.querySelector('.searchbar')
+
 searchbar.addEventListener('input', () => {
     filterCharacters()
+    searchAutoComplete()
 })
 
-const load_more_btn = document.querySelector('.load-more-btn')
 load_more_btn.addEventListener('click', () => {
     isLoadingMoreResults = true
     getCharacters()
+})
+
+// Click outside input search closing the aucomplete results
+document.addEventListener('click', e => {
+    let isClickInsideSearchbar = searchbar.contains(e.target)
+
+    if(!isClickInsideSearchbar) {
+        autocomplete_container.classList.remove('show-autocomplete')
+    }
 })
 
 /*========= Functions Calls ========= */
